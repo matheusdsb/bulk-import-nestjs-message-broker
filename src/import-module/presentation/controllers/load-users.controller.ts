f56f08e562c608bac2df5f19edacc6915cf7830d
Matheus Sabino <matheusdsb@gmail.com>
2023-03-04 03:01:14 -0300

    feat: use kafka as message broker

--- src/import-module/presentation/controllers/load-users.controller.ts
+++ src/import-module/presentation/controllers/load-users.controller.ts
@@ -1,14 +1,24 @@
-import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Controller, Inject, OnModuleInit } from '@nestjs/common';
+import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
 import { LoadUsersService } from '../../domain/use-cases/load-users/load-users.service';
 import { ImportFilter } from '../../domain/models/import-filter';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Controller('load-users')
-export class LoadUsersController {
-  constructor(private readonly loadUsersService: LoadUsersService) {}
+export class LoadUsersController implements OnModuleInit {
+  constructor(
+    private readonly loadUsersService: LoadUsersService,
+    @Inject('import-users-service') private readonly clientKafka: ClientKafka,
+  ) {}
 
   @MessagePattern('load-users')
   async loadUsers(@Payload() data: ImportFilter): Promise<void> {
     await this.loadUsersService.sendUsersForSaving(data);
+  }
+
+  async onModuleInit() {
+    this.clientKafka.subscribeToResponseOf('load-users');
+    await this.clientKafka.connect();
   }
 }
